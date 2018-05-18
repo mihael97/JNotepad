4217e7bb949a4ac5fe3cbbec46e50eb33cae880c
mihael97 <mihael.macuka2@gmail.com>
2018-05-18 16:34:56 +0200

    First commit

---
+++ src/main/java/hr/fer/zemris/java/hw11/jnotepadpp/JNotepadPP.java
@@ -0,0 +1,21 @@
+package hr.fer.zemris.java.hw11.jnotepadpp;
+
+/**
+ * Public class where starting program point is stored
+ * 
+ * @author Mihael
+ *
+ */
+public class JNotepadPP {
+
+	/**
+	 * Main program
+	 * 
+	 * @param args
+	 *            - not in use
+	 */
+	public static void main(String[] args) {
+
+	}
+
+}
